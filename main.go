3f04b03a94b870730d0af783e67f3bf2f432f913
Alexxtn105 <netmann105@gmail.com>
2024-12-08 11:52:39 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	//region Небуферизированный канал
+	fmt.Println("--------Небуферизированный канал---------")
+	orders := make(chan string)
+
+	// потребитель размещает заказы
+	go func() {
+		for i := 0; i <= 5; i++ {
+			order := fmt.Sprintf("Coffee order #%d", i)
+			orders <- order // горутина блокируется, пока бариста не примет заказ
+
+			fmt.Println("ORDER Placed:", order)
+		}
+
+		// закрываем канал
+		close(orders)
+	}()
+
+	// Бариста обрабатывает заказы
+	for order := range orders { // Читаем из канала. Это блокирующий вызов
+		fmt.Printf("Preparing: %s\n", order)
+		time.Sleep(2 * time.Second) // время на подготовку заказа
+		fmt.Printf("Served: %s\n", order)
+	}
+
+	//endregion
+
+	//region Буферизированный канал
+
+	fmt.Println("--------Буферизированный канал---------")
+
+	//endregion
+
+}
